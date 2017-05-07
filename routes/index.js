768c8f0a0b2539f5e6f4fad5812a62d0b3a9ba66
Nileesha Fernando <nileeshasajani@hotmail.com>
2017-05-07 13:52:29 -0400

    added login and signup pages

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.render('index');
+})
+router.get('/login', function(req, res){
+	res.render('login');
+})
+router.get('/signup', function(req, res){
+	res.render('signup');
+})
+
+
+module.exports = router;
